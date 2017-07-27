c8927630789dc3a654b161448ca3ec68e0b0ed3b
Robin Franken <rfranken.prive@gmail.com>
2017-07-27 20:54:28 +0200

    Created scenario
    Now it seems impossible to stub the `add` function that is exported from the utils.

--- test/anotherThing.spec.js
+++ test/anotherThing.spec.js
@@ -1,11 +1,16 @@
 import { expect } from 'chai';
+import { stub } from 'sinon';
 
 import * as anotherThing from '../src/anotherThing';
+import * as utils from '../src/utils';
 
 describe('anotherThing', () => {
     it('should add two things together', () => {
         const thing1 = 10;
         const thing2 = 20;
+
+        //This line does absolute nothing
+        stub(utils, 'add').returns(2);
 
         expect(anotherThing.addTwoThings(thing1, thing2)).to.equal(30);
     });
